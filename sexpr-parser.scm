4cf9912a182bbcfd2896ed922b8ef69367e93d86
Tal Amit <talamit40@gmail.com>
2017-11-04 12:05:35 -0700

    <sexpr> parsers complete

--- sexpr-parser.scm
+++ sexpr-parser.scm
@@ -283,7 +283,7 @@ done)
 		(*caten 3)
 
 		(*pack-with
-			(lambda(opem string close) 
+			(lambda(open string close) 
 				(list->string string)))
 
 	done)
@@ -291,6 +291,81 @@ done)
 )
 
 
+(define <ProperList>
+
+	(new
+		(*parser (char #\( ))
+		(*delayed (lambda() <sexpr>)) *star
+		(*parser (char #\) ))
+		(*caten 3)
+		(*pack-with
+			(lambda (pern1 sexp pern2) sexp ))
+
+	done)
+
+)
+
+(define <ImproperList>
+	(new
+		(*parser (char #\( ))
+		(*delayed (lambda() <sexpr>)) *plus
+		(*parser (char #\. ))
+		(*delayed (lambda() <sexpr>))
+		(*parser (char #\) ))
+		(*caten 5)
+		(*pack-with 
+			(lambda (pern1 sexplst dot sexp pern2) (append sexplst sexp)))
+
+	done)
+
+)
+
+(define <Vector>
+	(new
+		(*parser (char #\# ))
+		(*parser <ProperList>)
+		(*caten 2)
+		(*pack-with
+		(lambda (pre vec) vec))
+
+	done)
+
+)
+
+;catenate quote, unquote and quasiquote to <sexpr> 
+(define catenPreSexp
+	(lambda (pre str)
+	(new
+		(*parser (char pre))
+		(*parser <sexpr>)
+		(*caten 2)
+	(*pack-with
+		(lambda (pref sx) (list str sx)))
+	done) 
+	)
+)
+
+
+(define <Quoted>	
+		(catenPre #\' 'quote)
+)
+(define <QuasiQuoted>
+	(catenPre #\` 'quasiquote)
+)
+(define <Unquoted>
+	(catenPre #\, 'unquote)
+)
+
+
+(define <UnquoteAndSpliced>
+  (new 
+  	  (*parser (word ",@"))
+      (*parser <sexpr>)
+      (*caten 2)
+    (*pack-with
+        (lambda (pref sx) (list 'unquote-splicing sx)))
+done)
+)
 
 
 (define <InfixPrefixExtensionPrefix>
